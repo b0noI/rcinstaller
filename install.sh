f7e6e5b603b30f59424e0cb3654837437e730754
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2017-01-07 18:09:01 -0800

    New version of install that stores all config in .profile.d filoder.

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 ZSH_RC=".zshrc"
 BASH_RC=".bashrc"
+PROFILES_PATH="${HOME}/.profile.d"
+
+LINK_TO_BASH_RC="[ -f ${HOME}/.profile.d/*.sh ] && source ${HOME}/.profile.d/*.sh"
 
 COMMON_RC_FILE=".commonrc"
 if [ $# -eq 1 ]
@@ -9,28 +12,36 @@ if [ $# -eq 1 ]
   COMMON_RC_FILE=$1
 fi
 
-cp ./$COMMON_RC_FILE $HOME
-function link_commonrc() {
-  system_rc=$1
-  system_rc_path=$HOME"/"$system_rc
-  commonrc=$2
-  echo "Searching for $system_rc_path"
-  if [ -e "$system_rc_path" ]
+# Creates ${PROFILES_PATH} if it's not exsists already.
+function creat_conf_dir_if_needed() {
+  if [ ! -d "${PROFILES_PATH}" ]
   then
-    echo "$system_rc file has been found, checking if it already includes $commonrc"
-    if grep -Fxq "$system_rc_path" "$commonrc"
-    then
-     echo "looks like $commonrc need to be added. Adding."
-     echo "source ~/$commonrc" >> ~/$system_rc_path
-     echo "file $commonrc has been added to the $system_rc"
-     echo "$commonrc has been added to the $system_rc"
-    else
-     echo "$system_rc already has the $commonrc"
-    fi
-  else
-    echo "no $system_rc file has been found."
+    mkdir "${PROFILES_PATH}"
   fi
 }
 
-link_commonrc $ZSH_RC $COMMON_RC_FILE
-link_commonrc $BASH_RC $COMMON_RC_FILE
+# Adds linkt to the rc file, name of the rc file passed via the argument.
+function add_link_to_rcfile() {
+  system_rc=$1
+  system_rc_path=$HOME"/"$system_rc
+  echo "Searching for $system_rc_path"
+  if [ -e "$system_rc_path" ]
+  then
+    echo "$system_rc_path file has been found, checking if it already includes needed link: $LINK_TO_BASH_RC."
+    if grep -F "$LINK_TO_BASH_RC" "$system_rc_path"; then
+     echo "$system_rc already has the needed link: $LINK_TO_BASH_RC"
+    else
+     echo "looks like the link need to be added. Adding."
+     echo "${LINK_TO_BASH_RC}" >> $system_rc_path
+     echo "The link has been added to the $system_rc"
+    fi
+  else
+    echo "no $system_rc_path file has been found."
+  fi
+}
+
+creat_conf_dir_if_needed
+add_link_to_rcfile $ZSH_RC
+add_link_to_rcfile $BASH_RC
+cp $COMMON_RC_FILE $PROFILES_PATH
+echo "New RC has been installed, log out/in needed in order apply new changes."
